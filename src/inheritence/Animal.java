1f852fab177938018d74e9576c06360f69746a49
Isuru <nuwansnj99@gmail.com>
2024-11-22 13:16:12 +0530

    Animal class implementation

---
+++ src/inheritence/Animal.java
@@ -0,0 +1,41 @@
+package inheritence;
+
+public class Animal {
+
+    /*{
+        System.out.println("This is Animal Class");
+    }*/
+    private String name;
+    private int age;
+
+
+    public Animal(String name, int age, boolean eat) {
+        this.name = name;
+        this.age = age;
+
+
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void isEat() {
+        System.out.println(name + " is eating");
+    }
+
+
+}
